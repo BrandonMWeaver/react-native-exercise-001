aae57a3e93bd7781a52319d8e94a3678dc938723
Brandon Weaver <a7xncob@gmail.com>
2020-12-22 14:09:45 -0500

    Implement simple component state.

--- App.js
+++ App.js
@@ -1,14 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component {
+  state = {
+    count: 0
+  }
+
+  onIncrementPress = () => {
+    this.setState({
+      count: this.state.count + 1
+    });
+  }
+  
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>{this.state.count}</Text>
+        <Button onPress={this.onIncrementPress} title="increment" />
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
